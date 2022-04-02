eef2dd62ae5d9ec77859bf352b8850ac2d9963cf
roidujeu <sofogos@protonmail.com>
2022-04-02 15:51:44 +0530

    added play comps, other changes

--- app/src/main/java/org/biotstoiq/gophercle/SettingsActivity.java
+++ app/src/main/java/org/biotstoiq/gophercle/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.method.LinkMovementMethod;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -105,7 +104,5 @@ public class SettingsActivity extends Activity {
 
             srchUrlValET = alrtDlg.findViewById(R.id.adrUrlValET);
         });
-
-        exploreBQTV.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
